6e200fdde1fd2c006665c4c15fe4730156c6f529
msecowner <2202020202@qq.com>
2016-12-18 15:40:37 +0800

    update VOAClient.java

--- document/msec/examples/dev_java/Android/VOAClient.java
+++ document/msec/examples/dev_java/Android/VOAClient.java
@@ -1,12 +1,13 @@
 import MainLogic.VOAJavaMainLogic;
+import api.lb.msec.org.AccessLB;
+import api.lb.msec.org.Route;
 import org.msec.rpc.SrpcProxy;
+
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-/**
- * Created by yuyang on 2016/8/19.
- */
-public class VOAClient {
+public class Main {
+
     public static void main(String[] args) {
         SrpcProxy proxy = new SrpcProxy();
         proxy.setCaller("android_1.0");//调用者身份，用于日志展现
@@ -20,12 +21,38 @@ public class VOAClient {
         byte[] sendBytes = proxy.serialize(request);	//将请求包序列化为字节流，可通过网络发送。注意：序列化后的结果加上了SRPC的内部报文头
         VOAJavaMainLogic.GetTitlesResponse response;    //回包结构体
 
+
+
+
+
+
         //网络通信的基本工作
         Socket socket = new Socket();
-        String ip = ; // 服务器IP
+        String ip = ""; // 服务器IP
         int port = 7963;
 
         try {
+            /*
+             * MSEC v2.0 released some api to access LB
+             *
+             * client such as servlet or cgi can call these api to get IP and port of destination service, and
+             * if you do so, you must install LB agent on client server, and start it in 'client' mode. for example:
+             *                        ./nlbagent 10.104.95.110:8972 -m client -i eth0
+             * You can get detail information from nlbagent program
+             *
+             * Actually, this is not fixed situation, servlet or cgi may have its own load balance method, MSEC can
+             * not arrange their business.
+             *
+            AccessLB lb = new AccessLB();
+            Route r = new Route();
+
+            lb.getroutebyname("VOA_java.MainLogic", r);
+            ip = r.getIp();
+            port = r.getPort();
+
+            System.out.println("get server from LB:"+ip+":"+port);
+            */
+
             socket.setSoTimeout(20000);//20秒超时
             socket.connect(new InetSocketAddress(ip, port), 2000);  //连接服务
 
@@ -67,5 +94,5 @@ public class VOAClient {
         catch (Exception ex) {
             ex.printStackTrace();
         }
-   }
+    }
 }
