32def9edb6d465bb7c6b3acbb17617fd4a8eb1f8
msecowner <2202020202@qq.com>
2018-06-29 17:26:09 +0800

    security

--- msec_console/src/beans/service/QueryAlarmSetting.java
+++ msec_console/src/beans/service/QueryAlarmSetting.java
@@ -217,10 +217,13 @@ public class QueryAlarmSetting extends JsonRPCHandler {
         String svc = "";
         String date = "";
 
-        String result;
-
-
-
+        String result = checkIdentity();
+        if (!result.equals("success"))
+        {
+            response.setStatus(99);
+            response.setMessage(result);
+            return response;
+        }
 
         svc = request.getService_name();
         date = request.getDate();
